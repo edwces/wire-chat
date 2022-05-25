dbe48a675416ab6c2e9d6af0b8f53603431e8eab
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-05-25 14:00:38 +0200

    scrolling to the bottom at message

--- web/src/modules/chat/ChatSection.tsx
+++ web/src/modules/chat/ChatSection.tsx
@@ -6,6 +6,7 @@ import { MessageList } from "./MessageList";
 import { useChatRoom } from "./useChatRoom";
 import { useConversationReceivers } from "./useConversationReceivers";
 import { useConversationMessages } from "./useConversationMessages";
+import { useEffect, useRef } from "react";
 
 interface ChatSectionProps {
   id: number;
@@ -13,9 +14,17 @@ interface ChatSectionProps {
 
 export function ChatSection({ id }: ChatSectionProps) {
   useChatRoom(id);
+  const viewport = useRef<HTMLDivElement>();
   const userId = useAuthStatus((state) => state.id);
-  const messages = useConversationMessages(id);
+  const messages = useConversationMessages(id); // on new data: scroll to the bottom if user scrollbar is at the bottom
   const receiver = useConversationReceivers(id);
+
+  useEffect(() => {
+    const scrollbar = viewport.current;
+    // if ((scrollbar!.scrollHeight - scrollbar!.scrollTop) === scrollbar!.clientHeight) {
+    scrollbar?.scrollTo({ top: scrollbar.scrollHeight, behavior: "smooth" });
+    // }
+  }, [messages.data]);
 
   if (!messages.data || !receiver.data) return <div>Loading</div>;
 
@@ -24,7 +33,11 @@ export function ChatSection({ id }: ChatSectionProps) {
       <Stack sx={{ height: "95vh" }}>
         <ChatHeader name={receiver.data.name} image={receiver.data.avatar} />
         <Divider mb={10} />
-        <ScrollArea type="scroll" sx={{ flexGrow: 1, paddingRight: 24 }}>
+        <ScrollArea
+          type="scroll"
+          viewportRef={viewport}
+          sx={{ flexGrow: 1, paddingRight: 24 }}
+        >
           <MessageList data={messages.data} id={userId!} />
         </ScrollArea>
         <Divider mt={10} />
